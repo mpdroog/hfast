7c68881beac5a2a7939e46e001b281d1ac6de8c3
mark <mpdroog@rootdev.nl>
2018-08-30 18:11:38 +0200

    Feature. GZIP and Vary:Accept-Encoding for speed

--- main.go
+++ main.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"crypto/tls"
-	"fmt"
 	"github.com/VojtechVitek/ratelimit"
 	"github.com/coreos/go-systemd/daemon"
 	"golang.org/x/crypto/acme/autocert"
 	"log"
 	"net/http"
 	"time"
-	//"github.com/VividCortex/godaemon"
+	"github.com/NYTimes/gziphandler"
 )
 
 var assets = []string{
@@ -18,7 +17,7 @@ var assets = []string{
 
 func push(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("path=" + r.URL.Path)
+		w.Header().Set("Vary", "Accept-Encoding")
 		if r.URL.Path == "/" {
 			if pusher, ok := w.(http.Pusher); ok {
 				for _, asset := range assets {
@@ -27,7 +26,6 @@ func push(h http.Handler) http.Handler {
 						break
 					}
 				}
-				fmt.Println("Pushed assets")
 			}
 		}
 		h.ServeHTTP(w, r)
@@ -35,13 +33,12 @@ func push(h http.Handler) http.Handler {
 }
 
 func main() {
-	//godaemon.MakeDaemon(&godaemon.DaemonAttr{})
 	fs := http.FileServer(http.Dir("/var/www/usenet.today/pub"))
 
 	limit := ratelimit.Throttle(1)
 	mux := &http.ServeMux{}
-	mux.Handle("/action/", limit(NewHandler("/var/www/usenet.today/action/index.php", "tcp", "127.0.0.1:8000")))
-	mux.Handle("/", push(fs))
+	mux.Handle("/action/", gziphandler.GzipHandler(limit(NewHandler("/var/www/usenet.today/action/index.php", "tcp", "127.0.0.1:8000"))))
+	mux.Handle("/", gziphandler.GzipHandler(push(fs)))
 
 	m := &autocert.Manager{
 		Cache:      autocert.DirCache("/var/www/usenet.today/certs"),
