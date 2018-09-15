34819e25bd25f70a394cb61255aa2c2059225eb1
mark <mpdroog@rootdev.nl>
2018-09-15 18:30:17 +0200

    Feature. Dynamic vhost by convention

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"crypto/tls"
+	"fmt"
 	"github.com/VojtechVitek/ratelimit"
 	"github.com/coreos/go-systemd/daemon"
 	"golang.org/x/crypto/acme/autocert"
@@ -9,6 +10,7 @@ import (
 	"net/http"
 	"time"
 	"github.com/NYTimes/gziphandler"
+	"io/ioutil"
 )
 
 var assets = []string{
@@ -18,6 +20,8 @@ var assets = []string{
 func push(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Vary", "Accept-Encoding")
+		w.Header().Set("x-frame-options", "SAMEORIGIN")
+		w.Header().Set("x-xss-protection", "1; mode=block")
 		if r.URL.Path == "/" {
 			if pusher, ok := w.(http.Pusher); ok {
 				for _, asset := range assets {
@@ -32,26 +36,63 @@ func push(h http.Handler) http.Handler {
 	})
 }
 
-func main() {
-	fs := http.FileServer(http.Dir("/var/www/usenet.today/pub"))
+func vhost(muxs map[string]*http.ServeMux) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m, ok := muxs[r.Host]
+		if !ok {
+			log.Printf("Unmatched host: %s", r.Host)
+			w.Write([]byte("ERR: No such site."))
+			return
+		}
+		m.ServeHTTP(w, r)
+	}
+}
 
+func getDomains() ([]string, error) {
+	fileInfo, err := ioutil.ReadDir("/var/www/")
+	if err != nil {
+		return nil, err
+	}
+
+	out := []string{}
+	for _, file := range fileInfo {
+		if file.IsDir() {
+			out = append(out, file.Name())
+		}
+	}
+	return out, nil
+}
+
+func main() {
+	domains, e := getDomains()
+	if e != nil {
+		panic(e)
+	}
+
+	muxs := make(map[string]*http.ServeMux)
 	limit := ratelimit.Throttle(1)
-	mux := &http.ServeMux{}
-	mux.Handle("/action/", gziphandler.GzipHandler(limit(NewHandler("/var/www/usenet.today/action/index.php", "tcp", "127.0.0.1:8000"))))
-	mux.Handle("/", gziphandler.GzipHandler(push(fs)))
+	for _, domain := range domains {
+		fs := gziphandler.GzipHandler(push(http.FileServer(http.Dir(fmt.Sprintf("/var/www/%s/pub", domain)))))
+		action := gziphandler.GzipHandler(limit(NewHandler(fmt.Sprintf("/var/www/%s/action/index.php", domain), "tcp", "127.0.0.1:8000")))
+
+		mux := &http.ServeMux{}
+	        mux.Handle("/action/", action)
+		mux.Handle("/", fs)
+		muxs[domain] = mux
+	}
 
 	m := &autocert.Manager{
-		Cache:      autocert.DirCache("/var/www/usenet.today/certs"),
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("usenet.today"),
-	}
+                Cache:      autocert.DirCache("/tmp/certs"),
+                Prompt:     autocert.AcceptTOS,
+                HostPolicy: autocert.HostWhitelist(domains...),
+        }
 	s := &http.Server{
-		Addr:         ":443",
-		TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
-		Handler:      mux,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
+                Addr:         ":443",
+                TLSConfig:    &tls.Config{GetCertificate: m.GetCertificate},
+                Handler:      vhost(muxs),
+                ReadTimeout:  5 * time.Second,
+                WriteTimeout: 10 * time.Second,
+        }
 
 	go http.ListenAndServe("", m.HTTPHandler(nil))
 
