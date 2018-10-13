313b265a56fe455c0078aa21590eb7d0bae173e4
mark <mpdroog@rootdev.nl>
2018-10-13 16:49:09 +0200

    Feature. HTest (micro HTTP-server for testing action-logic)

---
+++ contrib/htest/main.go
@@ -0,0 +1,55 @@
+// HTest is a mini HTTP-server for unittesting
+// the action-code.
+package main
+
+import (
+	"fmt"
+	"flag"
+	"net/http"
+	"time"
+	"os"
+	"syscall"
+	"os/signal"
+)
+
+func fs(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	if _, e := w.Write([]byte("Regular page handle, you forgot to call /action!")); e != nil {
+		fmt.Printf("w.Write e=" + e.Error())
+	}
+}
+
+func mux(m *http.ServeMux) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		m.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	httpListen := ""
+	flag.StringVar(&httpListen, "l", ":8081", "HTTP iface:port (to override port 80 binding)")
+	flag.Parse()
+
+	dir, e := os.Getwd()
+		if e != nil {
+			fmt.Printf("getwd e=%s\n", e.Error())
+	}
+
+		mux := &http.ServeMux{}
+		mux.Handle("/action/", NewHandler(dir + "/action/index.php", "tcp", "127.0.0.1:8000"))
+		mux.HandleFunc("/", fs)
+
+	s := &http.Server{
+		Addr:         httpListen,
+		Handler:      mux,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	sigOS := make(chan os.Signal, 1)
+	signal.Notify(sigOS, os.Interrupt)
+	signal.Notify(sigOS, syscall.SIGTERM)
+
+	go s.ListenAndServe()
+	<-sigOS
+}
