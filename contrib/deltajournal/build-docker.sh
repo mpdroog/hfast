ec167f09cb6a25111c8e0247bca597e36518dbba
mark <mpdroog@rootdev.nl>
2018-12-12 13:31:30 +0100

    Bugfix. Bash strictmode, Bugfix. Docker aborts apt-get install due to confirmatio

--- contrib/deltajournal/build-docker.sh
+++ contrib/deltajournal/build-docker.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 # https://github.com/docker-library/docs/tree/master/golang
 # OLD docker run --rm -v "$PWD":/usr/src/deltajournal -w /usr/src/deltajournal golang:1.11 bash -c "
+set -euo pipefail
+IFS=$'\n\t'
 
 docker run --rm -v "$PWD":/go/src/deltajournal -w /go/src/deltajournal golang:1.11 bash -c "
+set -euo pipefail
+IFS=$'\n\t'
 /usr/bin/apt-get update
-apt-get install libsystemd-dev
+apt-get install -y libsystemd-dev
 go get
 go build"
 
