6f1742f2a6e31d8be4ef700186ba808585fcebe3
MP Droog <mpdroog@icloud.com>
2019-06-16 15:43:18 +0200

    Feature. Extend logparser with more metrics

--- contrib/logparser/main.go
+++ contrib/logparser/main.go
@@ -9,6 +9,9 @@ import (
     "flag"
     "log"
     "os"
+    "net"
+    "strings"
+    "time"
 )
 
 type Msg struct {
@@ -28,8 +31,21 @@ type Msg struct {
 }
 
 type Output struct {
-    Day map[string]uint64
+    Visitors map[string]map[string]uint64
+    StatusCodes map[string]map[int]int
     Referer map[string]uint64
+    Warnings map[string]Msg
+    LastUpdate string
+}
+
+func FormatMsg(m Msg) Msg {
+    if strings.HasPrefix(m.Referer, "https://www.paypal.com/webapps/hermes") {
+        m.Referer = "https://www.paypal.com/webapps/hermes"
+    }
+    if strings.HasPrefix(m.Referer, "https://usenet.today") {
+        m.Referer = ""
+    }
+    return m
 }
 
 func main() {
@@ -46,7 +62,9 @@ func main() {
     defer file.Close()
 
     uniqReferer := make(map[string]uint64)
-    uniqDay := make(map[string]uint64)
+    uniqDay := make(map[string]map[string]uint64)
+    dayStatusCodes := make(map[string]map[int]int)
+    warnings := make(map[string]Msg)
 
     msg := Msg{}
     scanner := bufio.NewScanner(file)
@@ -60,8 +78,23 @@ func main() {
     	if msg.Host != host {
     		continue
     	}
+        if msg.Status < 200 || msg.Status > 304 {
+            warnings[msg.URL] = msg
+        }
+        msg = FormatMsg(msg)
 
-    	uniqDay[msg.Date]++
+        host, _, e := net.SplitHostPort(msg.Remote)
+        if e != nil {
+            log.Fatal(e)
+        }
+        if _, ok := uniqDay[msg.Date]; !ok {
+            uniqDay[msg.Date] = make(map[string]uint64)
+        }
+        if _, ok := dayStatusCodes[msg.Date]; !ok {
+            dayStatusCodes[msg.Date] = make(map[int]int)
+        }
+    	uniqDay[msg.Date][host]++
+        dayStatusCodes[msg.Date][msg.Status]++
     	uniqReferer[msg.Referer]++
     }
 
@@ -70,8 +103,11 @@ func main() {
     }
 
     out := &Output{
-        Day: uniqDay,
+        Visitors: uniqDay,
+        StatusCodes: dayStatusCodes,
         Referer: uniqReferer,
+        Warnings: warnings,
+        LastUpdate: time.Now().Format("2006-01-02 15:04:05"),
     }
     enc := json.NewEncoder(os.Stdout)
     if e := enc.Encode(out); e != nil {
