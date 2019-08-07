b208c5bbeacd8f0801f71d0d519300ed2460c412
MP Droog <mpdroog@icloud.com>
2019-08-07 09:09:35 +0200

    Feature. AMP needs AMP-Redirect-To visible in CORS-res

--- main.go
+++ main.go
@@ -226,7 +226,7 @@ func CORS(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT")
-		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,AMP-Redirect-To")
 		if r.Method == "OPTIONS" {
 			w.Write([]byte("CORS :)"))
 			return
