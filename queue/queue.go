378ed012d820579673a528e2d3c857b4c3b6ad5c
mark <mpdroog@rootdev.nl>
2019-11-03 21:51:29 +0100

    Experimental. Queue-lib

---
+++ queue/queue.go
@@ -0,0 +1,148 @@
+/**
+ * Package queue implements a simple 'dynamic' queue, letting
+ * external services write to you so you can process them async
+ * and without any pressure to handle edge-cases like failure.
+ *
+ * - /queue/<name>/<MD5(secret+name)>(.ok)
+ *   <name> is channel to put HTTP-req in
+ *   (.ok) is optional and when set returns OK as body (if remote party needs this)
+ *   <secret> is key in overrides.toml
+ * - func health() returns health-status for queues
+ *   <name> = {pending: X, first: <when it started processing>, last: <last time job was processed> lastOK: <last time job was sucessfully processed>, ok=N err=N}
+ *
+ * This package uses boltdb to save the entries in buckets.
+ * /var/hfast.db > queue-bucket > name-bucket
+ */
+package queue
+
+import (
+	"crypto/md5"
+	"github.com/boltdb/bolt"
+	"net/http"
+	"sync"
+	"strings"
+	"encoding/json"
+	"encoding/binary"
+	"fmt"
+)
+
+var (
+	db *bolt.DB
+	state *sync.Map
+)
+
+type Job struct {
+	Id uint64
+}
+
+func Init() error {
+	state = new(sync.Map)
+
+	db, err := bolt.Open("/var/hfast.db", 0600, nil)
+	if err != nil {
+		return err
+	}
+
+	return db.View(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte("queue"))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+
+		c := b.Cursor()
+		for k, v := c.First(); k != nil; k, v = c.Next() {
+			fmt.Printf("key=%s, value=%s\n", k, v)
+			if v != nil {
+				return fmt.Errorf("bucket has values? val: %s", k)
+			}
+
+			cs := tx.Bucket(k).Cursor()
+			for k, _ := cs.First(); k != nil; k, _ = cs.Next() {
+				queue[string(v)] <- Msg{Id: k}
+			}
+		}
+
+		// TODO: Fill state machines so we're in the state we were
+		//  before restart.
+		return nil
+	})
+}
+
+func Req(w http.ResponseWriter, r *http.Request) {
+	// URL=/v1/url.json
+	toks := strings.SplitN(r.URL.Path, "/", 4)
+	if len(toks) != 4 {
+		w.WriteHeader(400)
+		w.Write([]byte("Invalid path, example=/queue/<channel>/MD5(secret+name)(.ok)\n"))
+		return
+	}
+
+	channel := toks[2]
+	hashArg := strings.SplitN(toks[3], ".", 2)
+	hash := hashArg[0]
+	expect := "SILENT"
+
+	if len(hashArg) > 1 {
+		expect = strings.ToLower(hashArg[1])
+		if expect != "ok" {
+			w.WriteHeader(400)
+			w.Write([]byte("Invalid return type, only .ok\n"))
+			return
+		}
+	}
+
+	/*
+		r.Header().Set("X-Domain", domain)
+		r.Header().Set("X-Secretkey", secretkey)
+	*/
+	domain := r.Header.Get("X-Domain")
+	secret := r.Header.Get("X-Secretkey")
+
+	if secret == "" {
+		panic("Missing secret-key")
+	}
+	if hash != fmt.Sprintf("%x", md5.Sum([]byte(secret + channel))) {
+		w.WriteHeader(403)
+		w.Write([]byte("Invalid hash.\n"))
+		return
+	}
+
+	e := db.Update(func(tx *bolt.Tx) error {
+		b, e := tx.CreateBucketIfNotExists([]byte("queue"))
+		if e != nil {
+			return e
+		}
+
+		bucketName := domain+"_"+channel
+		bchan, e := b.CreateBucketIfNotExists([]byte(bucketName))
+		if e != nil {
+			return e
+		}
+
+		id, e := bchan.NextSequence()
+		if e != nil {
+			return e
+		}
+		req, e := json.Marshal(r)
+		if e != nil {
+			return e
+		}
+		idb := []byte{}
+		binary.LittleEndian.PutUint64(idb, id)
+		if e := bchan.Put(idb, req); e != nil {
+			return e
+		}
+
+		queue[bucketName] <- Msg{Id: idb}
+		return nil
+	})
+	if e != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("Failed storing msg.\n"))
+		return
+	}
+
+	if expect == "ok" {
+		w.Write([]byte("OK"))
+	}
+}
---
+++ queue/worker.go
@@ -0,0 +1,135 @@
+/**
+ * TCP localhost:11300 for workers to connect to
+ * Every minute PING/PONG
+ * It's a server push protocol, the server will inform event-driven
+ * the request.
+ * > HELLO hfast.v1
+ * < CHAN chan1 chan2 ...
+ * > READY
+ * > PING
+ * < PONG
+ * > JOB <id> <byte_len>
+ * > ............
+ * > ............
+ * < DONE <id>
+ */
+package queue
+
+import (
+	"net"
+	"github.com/boltdb/bolt"
+	"log"
+	"bufio"
+	"time"
+	"strings"
+	"fmt"
+)
+
+type Msg struct {
+	Id []byte
+}
+
+var queue map[string]chan Msg
+var requeue map[string]chan Msg // re=retry
+
+// Limit amount of connected workers
+const WORKER_MAX = 100
+// Default listener
+const WORKER_LISTEN = "localhost:11300"
+
+// WARN: This function is blocking
+func Serve(listen string) error {
+	ln, e := net.Listen("tcp", listen)
+	if e != nil {
+		return e
+	}
+
+	for {
+		conn, e := ln.Accept()
+		if e != nil {
+			log.Printf("queue.Serve(%s) e=%s", listen, e.Error())
+			continue
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	defer conn.Close()
+	if _, e := conn.Write([]byte("HELLO hfast.v1\r\n")); e != nil {
+		log.Printf("queue.handle(HELLO) e=%s", e.Error())
+		return
+	}
+
+	lockDeadline := false
+	timer := time.NewTicker(time.Minute)
+	defer timer.Stop()
+
+	var channel string 
+	buf := bufio.NewReader(conn)
+	for {
+		msg, e := buf.ReadString('\n')
+		if e != nil {
+			// Erro reading
+			log.Printf("buf.ReadString e=%s", e.Error())
+			break
+		}
+		if !lockDeadline {
+			if e := conn.SetDeadline(time.Now().Add(2*time.Minute)); e != nil {
+				log.Printf("queue.SetDeadline e=%s", e.Error())
+				break
+			}
+		}
+		tok := strings.SplitN(msg, " ", 2)   // Limit 2 tokens
+		if tok[0] == "CHAN" {
+			channel = tok[1]
+			if _, e := conn.Write([]byte("READY\r\n")); e != nil {
+				log.Printf("queue.handle(CHAN) e=%s", e.Error())
+				break
+			}
+		} else if tok[0] == "PONG" {
+			// TODO: Add arg to prevent ahead of time messups
+		} else if tok[0] == "OK" {
+			// Job is processed
+			lockDeadline = false
+		} else {
+			if _, e :=  conn.Write([]byte("INVALID\r\n")); e != nil {
+				log.Printf("queue.handle(CHAN) e=%s", e.Error())
+			}
+			break
+		}
+
+		select {
+		case <-timer.C:
+			if _, e := conn.Write([]byte("PING\r\n")); e != nil {
+				log.Printf("queue.handle(PING) e=%s", e.Error())
+				break
+			}
+		case msg := <-queue[channel]:
+			// TODO: Memory friendly?
+			var v []byte
+			e := db.View(func(tx *bolt.Tx) error {
+				b := tx.Bucket([]byte("queue"))
+				bjob := b.Bucket([]byte(channel))
+				v = bjob.Get([]byte(msg.Id))
+				return nil
+			})
+			if e != nil {
+				log.Printf("queue.handle(msg) e=%s", e.Error())
+				break
+			}
+
+			if _, e := conn.Write([]byte(fmt.Sprintf("JOB %d %d\r\n", msg.Id, len(v)))); e != nil {
+				log.Printf("queue.handle(CHAN) e=%s", e.Error())
+				break
+			}
+
+			// Worker has 1-minute
+			lockDeadline = true
+			if e := conn.SetDeadline(time.Now().Add(time.Minute)); e != nil {
+				log.Printf("queue.SetDeadline e=%s", e.Error())
+				break
+			}
+		}
+	}
+}
