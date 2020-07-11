ca71bf04645cb0fc0713a8980477eb295076ff8b
mark <mpdroog@rootdev.nl>
2020-07-11 15:53:34 +0200

    buildsh

--- build.sh
+++ build.sh
@@ -5,5 +5,4 @@ set -u
 env GOOS=linux GOARCH=amd64 go build
 echo -n $'\003' | dd bs=1 count=1 seek=7 conv=notrunc of=./hfast
 
-echo "# scp -P2016 hfast root@104.248.142.101:~"
-scp hfast mark@192.168.178.44:~
+scp hfast fra1:~
