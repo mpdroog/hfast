b2839d8e3c4ef2bf0f31ecdbccbfe1ca0b8478c1
Mark Droog <mpdroog@icloud.com>
2023-01-09 19:53:52 +0100

    Patch. Switch smtp with mailgun for now

--- contrib/deltajournal/email.go
+++ contrib/deltajournal/email.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"deltajournal/config"
 	"fmt"
-	"gopkg.in/gomail.v1"
+	//"gopkg.in/gomail.v1"
+	"github.com/mailgun/mailgun-go/v4"
 	"math/rand"
 	"time"
 )
@@ -23,15 +25,30 @@ func RandText(n int) string {
 }
 
 func Email(e config.Email, body string) error {
-	msg := gomail.NewMessage()
-	msg.SetHeader("Message-ID", fmt.Sprintf("<%s@%s>", RandText(32), host))
-	msg.SetHeader("X-Mailer", "deltajournal")
-	msg.SetHeader("X-Priority", "3")
-	msg.SetHeader("From", e.Display+" <"+e.From+">")
-	msg.SetHeader("To", e.To...)
-	msg.SetHeader("Subject", fmt.Sprintf("[%s] %s", host, e.Subject))
-	msg.SetBody("text/plain", body)
+	// Create an instance of the Mailgun Client
+	mg := mailgun.NewMailgun(e.User, e.Pass)
+	mg.SetAPIBase("https://api.eu.mailgun.net/v3")
 
-	mailer := gomail.NewMailer(e.Host, e.User, e.Pass, e.Port)
-	return mailer.Send(msg)
+	message := mg.NewMessage(e.From, fmt.Sprintf("[%s] %s", host, e.Subject), body, e.To...)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	// Send the message with a 10 second timeout
+	_, _, err := mg.Send(ctx, message)
+	/*if config.Verbose {
+		fmt.Printf("ID: %s Resp: %s\n", id, resp)
+	}*/
+	return err
+
+	/*	msg := gomail.NewMessage()
+		msg.SetHeader("Message-ID", fmt.Sprintf("<%s@%s>", RandText(32), host))
+		msg.SetHeader("X-Mailer", "deltajournal")
+		msg.SetHeader("X-Priority", "3")
+		msg.SetHeader("From", e.Display+" <"+e.From+">")
+		msg.SetHeader("To", e.To...)
+		msg.SetHeader("Subject", fmt.Sprintf("[%s] %s", host, e.Subject))
+		msg.SetBody("text/plain", body)
+
+		mailer := gomail.NewMailer(e.Host, e.User, e.Pass, e.Port)
+		return mailer.Send(msg)*/
 }
